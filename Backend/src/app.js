26916fed54acfcd5d938103b5f770c2af2c10783
Asrar Ahammad <asrarahammadshaik@gmail.com>
2026-03-19 12:43:35 +0530

    config: Allow additional HTTP methods and headers in CORS middleware.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,6 +9,8 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
 
